ce9176b7d2423df6c83d09e511adb61630f4a5e5
Vitalii Sili <vitaliisili@yahoo.com>
2024-02-01 11:09:42 +0100

    backend

--- Django_Rest_Oauth2_Backend/apps/user/filters.py
+++ Django_Rest_Oauth2_Backend/apps/user/filters.py
@@ -2,4 +2,11 @@ from django_filters import rest_framework as filters
 
 
 class UserFilter(filters.FilterSet):
+    """ A filter class for filtering user data.
+    This class extends the `FilterSet` class from the `django_filters`
+    package and provides filtering options for user data.
+
+    Example:
+        To use the `UserFilter` class, you can define it in your view or serializer class:
+    """
     pass
